7642b337d7deb1c08d0dbeaec2f65f475f537f9a
00paning <163004918+00paning@users.noreply.github.com>
2024-12-10 09:06:12 +0800

    Add files via upload

---
+++ project2/Automatic Street Light Control.c
@@ -0,0 +1,35 @@
+#include <REGX51.H>
+sbit sensor1 = P1^0;
+sbit sensor2 = P1^1;
+sbit sensor3 = P1^2;
+
+sbit load1 = P2^0;
+sbit load2 = P2^1;
+sbit load3 = P2^2;
+
+void main()
+{
+	sensor1=sensor2=sensor3=0;
+	load1=load2=load3=0;
+	while(1)
+	{
+		if(sensor1==1)
+		{
+			load1=1;
+			load2=0;
+			load3=0;
+		}
+		if(sensor2==1)
+		{
+			load1=0;
+			load2=1;
+			load3=0;
+		}
+		if(sensor3==1)
+		{
+			load1=0;
+			load2=0;
+			load3=1;
+		}
+	}
+}
